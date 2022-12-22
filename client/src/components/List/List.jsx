c199731a9620944529aa94a810b0234aeae80347
Maykal Tenev <michaeltenev@gmail.com>
2022-12-22 20:10:40 +0100

    display list of fruits with material ui select form

--- client/src/components/List/List.jsx
+++ client/src/components/List/List.jsx
@@ -1,9 +1,21 @@
 import React, { useState, useEffect } from "react";
 import getAllProducts from "../../hooks/fetcher/getAllProducts.jsx";
 
+// Material UI Imports
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
 const List = () => {
   const [data, setData] = useState(null);
+  const [select, setSelected] = useState("");
 
+  const handleChange = (e) => {
+    setSelected(e.target.value);
+  };
+  console.log(select);
   useEffect(() => {
     const fetchData = async () => {
       const response = await getAllProducts();
@@ -11,17 +23,28 @@ const List = () => {
     };
     fetchData();
   }, []);
-
+  console.log(data);
   return (
     <div>
-      {data ? (
-        <ul>
-          {data?.map((product, i) => (
-            <li key={i}>{product?.name}</li>
-          ))}
-        </ul>
-      ) : (
-        <div>Loading...</div>
+      {data && (
+        <Box sx={{ minWidth: 120 }}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Fruit</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={select}
+              label="Fruit"
+              onChange={handleChange}
+            >
+              {data?.map((product, i) => (
+                <MenuItem key={i} value={product?.name}>
+                  {product?.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Box>
       )}
     </div>
   );
